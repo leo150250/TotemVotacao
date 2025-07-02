148f657f32ab30dca38937f0a71d1601a2111ff2
Leandro Gabriel - 2045189 <leandro.gabriel@ifro.edu.br>
2025-07-02 09:19:42 -0400

    Permite conexões externas

--- server/index.php
+++ server/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 // Define file path
 $filePath = __DIR__ . '/votos.json';
 
